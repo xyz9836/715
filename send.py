93ec5794b8b7ee1ea6a66a046361a920d6fc28d9
wsw <xyz9836@126.com>
2017-09-14 20:58:51 +0800

    1
    1

---
+++ send.py
@@ -0,0 +1,6 @@
+    import urllib2
+    import urllib
+    req = urllib2.urlopen(
+    url= 'https://sc.ftqq.com/SCU10361T0a2416cf6a6ca09da852bf223a588c2f59776131cef7d.send',
+    data= urllib.urlencode('text=IFTTT报告&desp=主人，服务器重启啦，检查一下吧~~ ')
+    )
