b7fcdeec9e74f995368800557f0b0a898efe20cb
wsw <xyz9836@126.com>
2017-09-16 12:18:42 +0800

    1

--- mailsand.py
+++ mailsand.py
@@ -1,17 +1,7 @@
-# -*- coding: utf-8 -*-
-import urllib
-import urllib2
-import requests
 from threading import Thread
 import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
-
-def send_offline():
-    test_data = {'text':'云监工报告','desp':'有设备状态异常'}
-    test_data_urlencode = urllib.urlencode(test_data)
-    requrl = "https://sc.ftqq.com/SCU10361T0a2416cf6a6ca09da852bf223a588c2f59776131cef7d.send"
-    req = urllib2.Request(url = requrl,data =test_data_urlencode)
 
 
 # 验证邮箱格式
